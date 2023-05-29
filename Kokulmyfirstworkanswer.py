01743a41f613491c9c4e0a123944e6174b87bce5
Kokul210 <134635928+Kokul210@users.noreply.github.com>
2023-05-29 21:03:12 +0530

    Add files via upload

---
+++ Kokulmyfirstworkanswer.py
@@ -0,0 +1,27 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python311/Kokulmyfirstwork.py
+Hello, World!
+What is your name? Kogul
+Hello  Kogul
+>>> 
+>>> age=12
+>>> print(age)
+12
+>>> 
+>>> Weight=40
+>>> print(weight)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(weight)
+NameError: name 'weight' is not defined. Did you mean: 'Weight'?
+>>> 
+>>> Weight=40
+>>> print(Weight)
+40
+>>> 
+>>> Height=142
+>>> print(Height)
+142
+>>> 
